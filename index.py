4a05833abdf40f214aea9167eba749606370c270
Ben Wu <wubin28@gmail.com>
2023-10-20 10:17:42 +0800

    feat: set cors header

--- index.py
+++ index.py
@@ -3,6 +3,7 @@ import tornado.ioloop
 
 class basicRequestHandler(tornado.web.RequestHandler):
     def get(self):
+        self.set_header("Access-Control-Allow-Origin", "https://www.husseinnasser.com")
         self.write(f"Served successfully.")
 
 if __name__ == "__main__":
